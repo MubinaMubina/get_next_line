db5c9ea35b0fcbdb093d7ddf9802df2625f729f9
Mubina Mubina <mmubina@z1t3c1.42bangkok.com>
2025-10-22 15:50:45 +0700

    added a new file

--- _fill_line_buffer.c
+++ _fill_line_buffer.c
@@ -6,7 +6,7 @@
 /*   By: mmubina <mmubina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 10:47:31 by mmubina           #+#    #+#             */
-/*   Updated: 2025/10/22 15:04:45 by mmubina          ###   ########.fr       */
+/*   Updated: 2025/10/22 15:47:23 by mmubina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 static char	*_fill_line_buffer(int fd, char *remainder, char *buffer)
 {
-	char *line_buffer;
-	int i;
-	static char *remainder;
-	ssize_t bytes_read;
+	char		*line_buffer;
+	int			i;
+	static char	*remainder;
+	ssize_t		bytes_read;
 
 	i = 0;
-	
-	while (bytes_read = read(fd, buffer, strlen(buffer)) > 0)
+	bytes_read = read(fd, buffer, strlen(buffer));
+	while (bytes_read > 0)
 	{
 		if (remainder[i] == NULL)
 			ft_strdup(remainder);
 		else
 			remainder = ft_strjoin(remainder, buffer);
 		if (buffer[i] == '\n')
-			break;
-		i++;	
+			break ;
+		i++;
 	}
 	return (remainder);
 }
