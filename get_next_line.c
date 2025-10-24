08b2451a330b4f3610272f57df9bf85d2a747745
Mubina Mubina <mmubina@z1t3c1.42bangkok.com>
2025-10-24 15:59:42 +0700

    added files

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mmubina <mmubina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 20:07:45 by mmubina           #+#    #+#             */
-/*   Updated: 2025/10/23 20:07:55 by mmubina          ###   ########.fr       */
+/*   Updated: 2025/10/24 15:58:49 by mmubina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,20 @@
 
 char	*get_next_line(int fd)
 {
+	static char	*remainder;
+	ssize_t		bytes_read;
+	char		*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (read(fd, 0, 0) < 0)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	remainder = _fill_line_buffer(fd, remainder, buffer);
+	line = _set_line(remainder);
+	free(buffer);
+	return (line);
 }
