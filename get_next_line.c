bc782c27f149d47e3e4cab6ac0ded4ef264a5ff4
Mubina Mubina <mmubina@z1t3c1.42bangkok.com>
2025-10-24 16:06:43 +0700

    added all

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mmubina <mmubina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 20:07:45 by mmubina           #+#    #+#             */
-/*   Updated: 2025/10/24 15:58:49 by mmubina          ###   ########.fr       */
+/*   Updated: 2025/10/24 16:05:55 by mmubina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 char	*get_next_line(int fd)
 {
 	static char	*remainder;
-	ssize_t		bytes_read;
 	char		*buffer;
 	char		*line;
 
@@ -27,7 +26,51 @@ char	*get_next_line(int fd)
 	if (!buffer)
 		return (NULL);
 	remainder = _fill_line_buffer(fd, remainder, buffer);
+	if (!remainder)
+	{
+		free(remainder);
+		return (NULL);
+	}
 	line = _set_line(remainder);
 	free(buffer);
 	return (line);
+}
+
+static char	*_fill_line_buffer(int fd, char *remainder, char *buffer)
+{
+	ssize_t	bytes_read;
+	char	*newline_found;
+
+	newline_found = NULL;
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	while (!newline_found && bytes_read > 0)
+	{
+		buffer[bytes_read] = '\0';
+		if (remainder == NULL)
+			remainder = ft_strdup(buffer);
+		else
+			remainder = ft_strjoin(remainder, buffer);
+		newline_found = ft_strchr(remainder, '\n');
+		if (newline_found)
+			break ;
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+	}
+	return (remainder);
+}
+
+char	*_set_line(char *line_buffer)
+{
+	int		i;
+	char	*remainder;
+
+	i = 0;
+	while (line_buffer[i] != '\0' && line_buffer[i] != '\n')
+		i++;
+	if (line_buffer[i] == '\n')
+	{
+		remainder = ft_substr(line_buffer, (i + 1), ft_strlen(line_buffer + i
+					+ 1));
+		line_buffer[i + 1] = '\0';
+	}
+	return (remainder);
 }
